a3b8aa2331e9e4814aa4e57cd492397bc244641d
lucas <lucas@MacBook-Pro-de-lucas.local>
2019-01-06 22:15:57 +0300

    Initial Commit

---
+++ iPrediction/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iPrediction
+//
+//  Created by lucas on 06/01/2019.
+//  Copyright © 2019 lucas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
